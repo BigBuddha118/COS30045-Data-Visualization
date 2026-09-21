a053a2c5602ae9dc694eda54b03ffe8c5ed9672e
Vaeshen Raj Jayandra <rajvaeshen@gmail.com>
2026-09-21 13:19:40 +0800

    D3 set up

---
+++ Exercise 4/Exercise 4.3/js/main.js
@@ -0,0 +1,16 @@
+// Exercise 4.3 - D3 set up
+
+// Step 2: create the responsive SVG canvas
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+      .attr("viewBox", "0 0 1200 1600")
+      .style("border", "1px solid black");
+
+// Step 3: add a test rectangle
+svg
+  .append("rect")
+    .attr("x", 10)
+    .attr("y", 10)
+    .attr("width", 414)
+    .attr("height", 16)
+    .attr("fill", "blue");
